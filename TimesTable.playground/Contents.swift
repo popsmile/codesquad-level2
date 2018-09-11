4d3bf95d35d8ec10924d1e421c8ec2fda1e09380
popsmile <popsmile7@gmail.com>
2018-09-11 16:00:01 +0900

    구구단 마무리

--- TimesTable.playground/Contents.swift
+++ TimesTable.playground/Contents.swift
@@ -55,3 +55,33 @@ func execute() {
 // 실행
 execute()
 
+//-------------------------------
+
+// 배열의 인덱스를 가져오는 indices 활용
+func gugudan1(number:Int) -> [Int] {
+    var gugudan = [Int].init(repeating:0, count:9)
+    for i in gugudan.indices {
+        gugudan[i] = number * (i+1)
+    }
+    return gugudan
+}
+
+// 배열 요소값을 0이 아닌 1부터 9로 채워서 시작
+func gugudan2(number:Int) -> [Int] {
+    var gugudan = Array(1...9)
+    for i in gugudan {
+        gugudan[i-1] = gugudan[i-1] * number
+    }
+    return gugudan
+}
+
+// gugudan 배열 프린트하여 확인
+func printGugudan(gugudan:[Int]) {
+    for i in gugudan {
+        print(i)
+    }
+}
+
+// 위 방법 두 가지로 각각 구구단결과값 확인해보기
+printGugudan(gugudan: gugudan1(number: 9))
+printGugudan(gugudan: gugudan2(number: 9))
